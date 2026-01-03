69c4e15dd984328f848cdaba2b8b9bd60451ceaa
Vincent Damewood <git@vdamewood.com>
2026-01-02 19:48:04 -0800

    Update copyright

--- src/Token.cc
+++ src/Token.cc
@@ -1,19 +1,22 @@
-/* Token.cc: Token passed from tokenizer to parser
- * Copyright 2012-2025 Vincent Damewood
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library. If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2012-2026 Vincent Damewood
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+// This file is part of Silikego.
+
+// Silikego is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published 
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Silikego is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with Silikego. If not, see
+// <http://www.gnu.org/licenses/>.
+
 
 #include <string>
 #include <variant>
