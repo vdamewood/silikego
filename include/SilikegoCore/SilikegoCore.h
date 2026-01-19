180a85986ea4b98eec6595d3fef3eee5fe776f56
Vincent Damewood <git@vdamewood.com>
2026-01-19 02:32:50 -0800

    Add all-inclusive header

---
+++ include/SilikegoCore/SilikegoCore.h
@@ -0,0 +1,36 @@
+// Copyright 2012-2026 Vincent Damewood
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+// This file is part of Silikego.
+
+// Silikego is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Silikego is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with Silikego. If not, see
+// <http://www.gnu.org/licenses/>.
+
+#if !defined SILIKEGO_CORE_SILIKEGO_CORE_H
+#define SILIKEGO_CORE_SILIKEGO_CORE_H
+
+#include <SilikegoCore/Api.h>
+#include <SilikegoCore/Evaluate.h>
+#include <SilikegoCore/Function.h>
+#include <SilikegoCore/Input.h>
+#include <SilikegoCore/Lexer.h>
+#include <SilikegoCore/Math.h>
+#include <SilikegoCore/Node.h>
+#include <SilikegoCore/Operator.h>
+#include <SilikegoCore/ParseInfix.h>
+#include <SilikegoCore/Token.h>
+#include <SilikegoCore/Value.h>
+#include <SilikegoCore/Version.h>
+
+#endif // SILIKEGO_CORE_SILIKEGO_CORE_H
